297d456553a48c6f318925857355566e39f022f4
Jonathan0x539 <jordison@protonmail.com>
2024-10-02 07:47:33 -0300

    chore: update index.js to import routes from separate files

--- index.js
+++ index.js
@@ -5,8 +5,18 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 // Importar as rotas
-const carRoutes = require('./routes/cars');
-app.use('/api/v1/cars', carRoutes);
+const createCar = require('./routes/createCar');
+const deleteCar = require('./routes/deleteCar');
+const getCar = require('./routes/getCar');
+const listCars = require('./routes/listCars');
+const updateCar = require('./routes/updateCar');
+
+// Usar as rotas
+app.use('/api/v1/cars', createCar);
+app.use('/api/v1/cars', deleteCar);
+app.use('/api/v1/cars', getCar);
+app.use('/api/v1/cars', listCars);
+app.use('/api/v1/cars', updateCar);
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
