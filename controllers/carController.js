783ae5c3c90d92baa4052682ca8dd805ff0f42c6
Jonathan0x539 <jordison@protonmail.com>
2024-10-04 14:56:58 -0300

    feat: implement CRUD operations in car controller

--- controllers/carController.js
+++ controllers/carController.js
@@ -19,3 +19,51 @@ exports.createCar = async (req, res) => {
     return res.status(error.status).json({ error: error.message });
   }
 };
+// function deleteCar
+exports.deleteCar = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    await carService.deleteCar(id);
+    return res.status(204).send();
+  } catch (error) {
+    return res.status(error.status).json({ error: error.message });
+  }
+};
+
+// function getCar
+exports.getCar = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const car = await carService.getCar(id);
+    return res.json(car);
+  } catch (error) {
+    return res.status(error.status).json({ error: error.message });
+  }
+};
+// finction listCars
+
+exports.listCars = async (req, res) => {
+  const { page = 1, limit = 5, brand, model, year } = req.query;
+
+  try {
+    const response = await carService.listCars(page, limit, brand, model, year);
+    return res.status(200).json(response);
+  } catch (error) {
+    return res.status(error.status).json({ error: error.message });
+  }
+};
+// function updateCar
+
+exports.updateCar = async (req, res) => {
+  const { id } = req.params;
+  const { brand, model, year, items } = req.body;
+
+  try {
+    await carService.updateCar(id, { brand, model, year, items });
+    return res.status(204).send();
+  } catch (error) {
+    return res.status(error.status).json({ error: error.message });
+  }
+};
